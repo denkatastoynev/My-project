81396c798250f92b7db654d7154c76bc496f53ed
denkatastoynev <denkatastoynev@gmail.com>
2024-02-19 22:54:54 +0200

    ADP PERF

---
+++ Assets/PrintToConsole.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms.GameCenter;
+
+public class PrintToConsole : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    GameObject game = new GameObject ();
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
